cdb065dd8c5070dd28b8f81e04e3d3fc984cab18
yegang0413 <yegangwoo0413@gmail.com>
2025-12-04 21:22:51 +0900

    베이스 링크 생성

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: '/study-buddy-bright-76/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
